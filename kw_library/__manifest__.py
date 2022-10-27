4e0d8bee68307a005221549bbccb201e9fd8d47a
Grigoriy Kosarev <grigoriykosarev@gmail.com>
2022-10-27 07:46:52 -0700

    init

---
+++ kw_library/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Library",
+
+    'summary': '',
+
+    'author': "Kitworks Systems",
+    'website': 'https://kitworks.systems/',
+
+    'category': 'Customizations',
+    'license': 'OPL-1',
+    'version': '15.0.1.0.0',
+
+    'depends': [
+
+    ],
+
+    'external_dependencies': {'python': [], },
+
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/book.xml',
+    ],
+    'demo': [
+    ],
+
+    'installable': True,
+    'auto_install': False,
+
+    'images': [
+
+    ],
+
+    'price': 0,
+    'currency': 'EUR'
+
+}
